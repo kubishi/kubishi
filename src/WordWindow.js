a4873af07d5976c89a0cf35e43ca5e3cbfaa2e94
Jared Coleman <jaredraycoleman@gmail.com>
2020-04-30 21:33:19 -0700

    Adds ability to save changes to words

--- src/WordWindow.js
+++ src/WordWindow.js
@@ -1,13 +1,34 @@
 
 import React from 'react';
-import { Row, Col, ListGroup } from 'react-bootstrap';
+import { Row, Col, ListGroup, Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 import { API_URL, API_KEY } from './env';
 
 const api = axios.create({
     baseURL: API_URL,
-})
+});
+
+const parts_of_speech = [
+    'NOUN',
+    'VERB',
+    'IDIOM',
+    'ADVERB',
+    'PRONOUN',
+    'ADJECTIVE',
+    'NOUN_SUFFIX',
+    'VERB_SUFFIX',
+    'PREPOSITION',
+    'POSTPOSITION',
+    'UNKNOWN',
+];
+
+const UserType = {
+  USER: 1,
+  EDITOR: 2,
+  DEVELOPER: 3,
+  ADMIN: 4
+};
 
 class WordWindow extends React.Component {
     constructor(props) {
@@ -61,8 +82,73 @@ class WordWindow extends React.Component {
         }).catch(err => console.error(err));
     }
 
+    /**
+     * 
+     * @param {String} e 
+     */
+    changePartOfSpeech(part_of_speech) {
+        this.setState({part_of_speech: part_of_speech.toUpperCase().replace(' ', '_')});
+    }
+
+    /**
+     * 
+     * @param {String} text 
+     */
+    changeWordText(text) {
+        this.setState({text: text});
+    }
+
+    /**
+     * 
+     * @param {String} text 
+     */
+    changeDefinition(definition) {
+        this.setState({definition: definition});
+    }
+
+    handleSave(e) {
+        let { text, part_of_speech, definition } = this.state;
+
+        let body = {};
+        if (text != null) body.text = text;
+        if (part_of_speech != null) body.part_of_speech = part_of_speech;
+        if (text != null || part_of_speech != null) {
+            api.put('/api/word/' + this.props.wordId, body, {
+                headers: {api_key: API_KEY}
+            }).then(res => {
+                if (res.status == 200) {
+                    this.getWord(this.props.wordId);
+                } else {
+                    console.log(res.status, res.data);
+                }
+            }).catch(err => console.error(err));
+        }
+        
+        if (definition != null) {
+            api.put('/api/word/' + this.props.wordId + '/definition',
+                {'text': definition}, {headers: {api_key: API_KEY}}
+            ).then(res => {
+                if (res.status == 200) {
+                    this.getWord(this.props.wordId);
+                } else {
+                    console.log(res.status, res.data);
+                }
+            }).catch(err => console.error(err));
+        }
+    }
+
+    canEdit() {
+        let user = this.props.getUser();
+        if (user != null && UserType[user.type] != null && UserType[user.type] >= UserType.EDITOR) {
+            return true;
+        }
+        return false;
+    }
+
     render() {
         let { word, suggestedSentences } = this.state;
+        let edit_mode = this.canEdit();
+
         if (word == null) {
             return null;
         } else if (word == false) {
@@ -73,14 +159,30 @@ class WordWindow extends React.Component {
             );
         }
 
+        let user_options = parts_of_speech.map((part_of_speech, i) => {
+            let pos = part_of_speech.toLowerCase().replace('_', ' ');
+            return (
+                <option>{pos}</option>
+            );
+        });
+
         let sentence_ids = word.sentences.map((sentence, i) => sentence._id);
         let sentences = word.sentences.map((sentence, i) => {
-            return (
-                <ListGroup.Item>
-                    <b>{sentence.text}</b>
-                    <p>{sentence.translation.text}</p>
-                </ListGroup.Item>
-            );
+            if (edit_mode) {
+                return (
+                    <ListGroup.Item>
+                        <b>{sentence.text}</b>
+                        <p>{sentence.translation.text}</p>
+                    </ListGroup.Item>
+                );
+            } else {
+                return (
+                    <ListGroup.Item>
+                        <b>{sentence.text}</b>
+                        <p>{sentence.translation.text}</p>
+                    </ListGroup.Item>
+                );
+            }
         });
         
         let sentencesList = null;
@@ -123,16 +225,70 @@ class WordWindow extends React.Component {
             );
         }
 
-        let part_of_speech = word.part_of_speech.toLowerCase().replace('_', ' ');
+        let wordPart;
+        let part_of_speech_option = word.part_of_speech.toLowerCase().replace('_', ' ');
+        let { text, part_of_speech, definition } = this.state;
+        let hasChanged = (
+            (text != null && text != word.text) ||
+            (part_of_speech != null && part_of_speech != word.part_of_speech) ||
+            (definition != null && definition != word.definition.text)
+        );
+        if (edit_mode) {
+            wordPart = (
+                <div>
+                    <Form>
+                        <Form.Group controlId='formWord'>
+                            <Form.Label>Word</Form.Label>
+                            <Form.Control 
+                                type='text' defaultValue={word.text}
+                                onChange={e => {this.changeWordText(e.target.value)}}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId='formPOS'>
+                            <Form.Label>Part of Speech</Form.Label>
+                            <Form.Control 
+                                as="select" 
+                                defaultValue={part_of_speech_option} 
+                                onChange={e => {this.changePartOfSpeech(e.target.value)}}
+                            >
+                                {user_options}
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='formDefinition'>
+                            <Form.Label>Definition</Form.Label>
+                            <Form.Control as="textarea" defaultValue={word.definition.text} 
+                                onChange={e => this.changeDefinition(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Button 
+                            variant={hasChanged ? 'outline-primary' : 'outline-secondary'} 
+                            block href='#'
+                            disabled={!hasChanged} 
+                            onClick={e => this.handleSave(e)}
+                        >Save</Button>
+                    </Form>
+                </div>
+            );
+        } else {
+            wordPart = (
+                <div>
+                    <h4>{word.text}</h4>
+                    <p><em>{part_of_speech_option}</em></p>
+                    <p>{word.definition.text}</p>
+                </div>
+            );
+        }
+
         return (
             <Row className='m-3'>
                 <Col 
                     xs={4}
-                    style={{paddingRight: '20px', borderRight: '1px solid #ccc'}}
+                    style={{'paddingRight': '20px', 'borderRight': '1px solid #ccc'}}
                 >
-                    <h4>{word.text}</h4>
-                    <p><em>{part_of_speech}</em></p>
-                    <p>{word.definition.text}</p>
+                    {wordPart}
                 </Col>
                 <Col>
                     {sentencesList}
