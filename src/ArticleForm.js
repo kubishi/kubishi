0e5d91f261047701edda1f75172f3c19a52d3645
Jared Coleman <jaredraycoleman@gmail.com>
2020-06-30 10:18:40 -0700

    Adds ability to upload images to article

--- src/ArticleForm.js
+++ src/ArticleForm.js
@@ -120,41 +120,31 @@ class App extends React.Component {
                     menubar: 'file edit view insert format tools table help',
                     toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
                     toolbar_sticky: true,
-                    autosave_ask_before_unload: true,
-                    autosave_interval: "30s",
-                    autosave_prefix: "{path}{query}-{id}-",
-                    autosave_restore_when_empty: false,
-                    autosave_retention: "2m",
                     image_advtab: true,
-                    content_css: '//www.tiny.cloud/css/codepen.min.css',
-                    link_list: [
-                    { title: 'My page 1', value: 'http://www.tinymce.com' },
-                    { title: 'My page 2', value: 'http://www.moxiecode.com' }
-                    ],
-                    image_list: [
-                    { title: 'My page 1', value: 'http://www.tinymce.com' },
-                    { title: 'My page 2', value: 'http://www.moxiecode.com' }
-                    ],
-                    image_class_list: [
-                    { title: 'None', value: '' },
-                    { title: 'Some class', value: 'class-name' }
-                    ],
                     importcss_append: true,
-                    file_picker_callback: function (callback, value, meta) {
-                    /* Provide file and text for the link dialog */
-                    if (meta.filetype === 'file') {
-                        callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
-                    }
-                
-                    /* Provide image and alt text for the image dialog */
-                    if (meta.filetype === 'image') {
-                        callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
-                    }
-                
-                    /* Provide alternative source and posted for the media dialog */
-                    if (meta.filetype === 'media') {
-                        callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
-                    }
+                    
+                    file_picker_callback: (cb, value, meta) => {
+                        var input = document.createElement('input');
+                        input.setAttribute('type', 'file');
+                        input.setAttribute('accept', 'image/*');
+
+                        input.onchange = () => {
+                            var file = this.files[0];
+                            var reader = new FileReader();
+                            reader.onload = function () {
+                                var id = 'blobid' + (new Date()).getTime();
+                                var blobCache =  window.tinymce.activeEditor.editorUpload.blobCache;
+                                var base64 = reader.result.split(',')[1];
+                                var blobInfo = blobCache.create(id, file, base64);
+                                blobCache.add(blobInfo);
+                        
+                                /* call the callback and populate the Title field with the file name */
+                                cb(blobInfo.blobUri(), { title: file.name });
+                            };
+                            reader.readAsDataURL(file);
+                        };
+                    
+                        input.click();
                     },
                     templates: [
                         { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
