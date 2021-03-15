7308a73030934597b69e059359f8e58b34ae6448
Jared Coleman <jaredraycoleman@gmail.com>
2021-03-15 08:01:33 -0700

    Updates help

--- src/info/Help.js
+++ src/info/Help.js
@@ -5,6 +5,22 @@ function PrivacyPolicy(props) {
     return (
         <div className="mt-2">
             <h2>Help</h2>
+            <h4 id="help-create">How do I create an account?</h4>
+            <p>
+                From the menu, select "Log in with Facebook" and follow the prompts to sign in with Facebook.
+                Currently, we only support logging in through Facebook.
+            </p>
+
+            <h4 id="help-create-list">How do I create lists?</h4>
+            <p>
+                From the menu, select "My Lists". Then, at the bottom of the page, click the button that says "New Word List".
+            </p>
+
+            <h4 id="help-create-list">How do I add words to my list?</h4>
+            <p>
+                Look up a word and go to its page, then look for the dropdown menu that says "Add Word to List".
+                Click it and select the list you would like to add the word to.
+            </p>
 
             <h4 id="help-delete">How do I delete my account?</h4>
             <p>
